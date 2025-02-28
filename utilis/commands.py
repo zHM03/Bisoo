b613cf6dda24d1f5037d3be304cedd228e850b40
Hisham <92490723+zHM03@users.noreply.github.com>
2025-02-28 03:46:01 +0300

    Update commands.py

--- utilis/commands.py
+++ utilis/commands.py
@@ -14,29 +14,29 @@ class Commands(commands.Cog):
     async def help(self, ctx):
         """Yardım komutunu sağlar"""
         embed = discord.Embed(
-            title="Biso'nun yetenekleri", 
+            title="Bisooo'nun Yetenekleri", 
             description="Biso ile neler yapabilirsiniz:", 
-            color=discord.Color.orange()  # Embed için mavi renk
+            color=discord.Color.orange()  # Embed için kedici bir renk
         )
-
+    
         help_message = (
-            "**!p:** *<Şarkı ismi, YouTube veya Spotify linki> şarkınızı çalarım*\n"
-            "**!s:** *Şarkınızı durdururum*\n"
-            "**!r:** *Şarkınızı devam ettiririm*\n"
-            "**!l:** *Yanınızdan ayrılırım :(*\n"
+            "**!p:** *<Şarkı ismi, YouTube linki> Mırlayarak şarkınızı çalarım*\n"
+            "**!s:** *Mırlamayı durdururum, sessiz olurum*\n"
+            "**!r:** *Mırlamaya devam ederim*\n"
+            "**!l:** *Yanınızdan ayrılırım, tüylerimle veda ederim :(*\n"
             "----------------------------------------------------\n"
             "**Oyun Takip Etme Komutları:**\n"
-            "**!addgame:** *<Oyun adı veya steam linki> listeye eklerim ve indirime girdğinde size miyavlarım*\n"
-            "**!listgames:** *Takip listesindeki oyunları gösteririm*\n"
-            "**!removegame:** *<Oyun adı>Listeden kaldırırım\n"
+            "**!addgame:** *<Oyun adı veya steam linki> Listeye eklerim ve indirime girince size miyavlarım*\n"
+            "**!listgames:** *Takip ettiğim oyunları liste olarak çıkarırım*\n"
+            "**!removegame:** *<Oyun adı> Oyun listemden silinir*\n"
             "----------------------------------------------------\n"
             "**Diğer Komutlar:**\n"
-            "**!h:** *<Şehir> Merak ettiğiniz şehrin hava durumunu söylerim*\n"
-            "**!j:** *Komik şakalar yaparım*\n"
-            "**!kedy:** *Arkadaşlarımı ifşalarım*\n"
-            "**!crypto:** *<coin> coin'in fiyatını gösterebilirim*\n"
+            "**!h:** *<Şehir> Merak ettiğiniz şehrin hava durumunu tüylerimle okurum*\n"
+            "**!j:** *Komik kedili şakalar yaparım*\n"
+            "**!kedy:** *Arkadaşlarımın yaramazlıklarını ifşalarım*\n"
+            "**!crypto:** *<coin> coin'in fiyatını patilerimle gösteririm*\n"
             "----------------------------------------------------\n"
-            "***Şimdilik bu kadarrr***"
+            "***Şimdilik bu kadarrr, kedice bye bye!***"
         )
         
         # Yardım mesajını embed'in içerisine ekliyoruz
@@ -44,6 +44,7 @@ class Commands(commands.Cog):
         
         # Yardım mesajını embed olarak gönder
         await ctx.send(embed=embed)
+
 
 async def setup(bot):
     await bot.add_cog(Commands(bot))
