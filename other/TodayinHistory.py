0b3096399ed9133476229666433546a46602d360
Hisham <92490723+zHM03@users.noreply.github.com>
2025-03-02 09:20:40 +0300

    Update TodayinHistory.py

--- other/TodayinHistory.py
+++ other/TodayinHistory.py
@@ -16,9 +16,9 @@ class TodayHistory(commands.Cog):
 
             # API'ye istek gönder
             url = f"https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/{month}/{day}"
-            response = await self.get_api_data(url)  # Asenkron veri çekme
+            response = requests.get(url, headers={"User-Agent": "bisooo/1.0 (h2kh03@gmail.com)"})
             
-            if response:
+            if response.status_code == 200:
                 data = response.json()
 
                 # Olayları kontrol et ve gönder
@@ -37,24 +37,11 @@ class TodayHistory(commands.Cog):
                 else:
                     await ctx.send("Bugün tarihteki olaylar bulunamadı.")
             else:
-                await ctx.send("API ile iletişim kurulamıyor.")
-
+                await ctx.send(f"API ile iletişim kurulamıyor. Hata kodu: {response.status_code}")
+        
         except Exception as e:
             print(f"Hata: {e}")
             await ctx.send("Bir hata oluştu, lütfen tekrar deneyin.")
-
-    # API'den asenkron veri çekme işlemi
-    async def get_api_data(self, url):
-        try:
-            headers = {"User-Agent": "bisooo/1.0 (h2kh03@gmail.com)"}
-            response = await self.bot.loop.run_in_executor(None, lambda: requests.get(url, headers=headers))
-            if response.status_code == 200:
-                return response
-            else:
-                return None
-        except Exception as e:
-            print(f"API hata: {e}")
-            return None
 
 # Cog'u bot'a eklemek için gerekli fonksiyon
 async def setup(bot):
