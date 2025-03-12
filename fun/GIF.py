896067a4571e44b229149fb2481602989af1dead
Hisham <92490723+zHM03@users.noreply.github.com>
2025-03-12 06:57:55 +0300

    Update GIF.py

--- fun/GIF.py
+++ fun/GIF.py
@@ -8,6 +8,8 @@ import os
 # .env dosyasını yükle
 load_dotenv()
 
+KANAL_ID = 1340760164617424938  # Komutun çalışmasını istediğiniz kanalın ID'si
+
 class Giphy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -17,7 +19,7 @@ class Giphy(commands.Cog):
     def get_random_gif(self, data):
         """Verilen Giphy API verisinden rastgele bir GIF seçer, daha önce gönderilmemişse seçer."""
         available_gifs = [gif for gif in data['data'] if gif['id'] not in self.sent_gifs]
-        
+
         if available_gifs:
             return random.choice(available_gifs)
         else:
@@ -28,33 +30,44 @@ class Giphy(commands.Cog):
         if len(self.sent_gifs) > self.max_sent_gifs:
             self.sent_gifs = self.sent_gifs[-self.max_sent_gifs:]
 
-    @commands.command(name='kedy')  # Komut adı 'gif' olarak belirledik
+    @commands.command(name='kedy')  # Komut adı 'kedy' olarak belirledik
     async def gif(self, ctx):
         """Rastgele kedi GIF'i gönderir"""
         
+        if ctx.channel.id != KANAL_ID:  # Eğer komut belirlenen kanalda değilse
+            kanal = ctx.guild.get_channel(KANAL_ID)
+            embed = discord.Embed(
+                title="Hrrrr!",
+                description=f"Lütfen {kanal.mention}'de buluşalım. Kediler burada mutlu! 😸",
+                color=discord.Color.red()
+            )
+            embed.set_footer(text="Bisonun keyfi 🐾")
+            await ctx.send(embed=embed)
+            return
+
         GIPHY_API_KEY = os.getenv('GIPHY_API_KEY')  # .env dosyasından API anahtarını al
         GIPHY_API_URL = 'https://api.giphy.com/v1/gifs/search'
-        
+
         query = 'cat'  # Kedi GIF'leri arıyoruz
         params = {
             'api_key': GIPHY_API_KEY,
             'q': query,
-            'limit': 2000  # Birden fazla sonuç alıyoruz, örneğin 10 sonuç
+            'limit': 50  # Çok fazla sonuç almaya gerek yok, 50 yeterli
         }
-        
+
         # Giphy API'den gelen veriyi çekiyoruz
         response = requests.get(GIPHY_API_URL, params=params)
-        
+
         if response.status_code == 200:
             data = response.json()
             if data['data']:  # Eğer sonuç varsa
                 self.clean_sent_gifs()  # Eski GIF'leri temizle
                 random_gif = self.get_random_gif(data)  # Yeni bir GIF seç
                 gif_url = random_gif['url']
-                
+
                 # Seçilen GIF'in ID'sini kaydediyoruz
                 self.sent_gifs.append(random_gif['id'])
-                
+
                 await ctx.send(gif_url)  # GIF URL'sini gönder
             else:
                 await ctx.send("Üzgünüm, kedi GIF'i bulunamadı.")
