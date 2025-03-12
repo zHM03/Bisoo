72d551f935f656d177811aee0cf6135e8f6f417b
Hisham <92490723+zHM03@users.noreply.github.com>
2025-03-12 06:58:57 +0300

    Update Joke.py

--- fun/Joke.py
+++ fun/Joke.py
@@ -4,12 +4,14 @@ import random
 import json
 import os
 
+KANAL_ID = 1340760164617424938  # Komutun çalışmasını istediğiniz kanalın ID'si
+
 class Joke(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     def load_jokes(self):
-        """JSON dosyasındaki şakaları yükle"""
+        """JSON dosyasındaki şakaları yükler"""
         file_path = os.path.join(os.path.dirname(__file__), 'jokes.json')
         with open(file_path, 'r', encoding='utf-8') as file:
             return json.load(file)
@@ -17,6 +19,18 @@ class Joke(commands.Cog):
     @commands.command(name='j')  # Komut adı 'j' olarak belirledik
     async def joke(self, ctx):
         """Rastgele şaka getirir"""
+
+        if ctx.channel.id != KANAL_ID:  # Yanlış kanalda çalıştırılırsa
+            kanal = ctx.guild.get_channel(KANAL_ID)
+            embed = discord.Embed(
+                title="Hrrrr!",
+                description=f"Lütfen {kanal.mention}'de buluşalım. Kediler burada mutlu! 😸",
+                color=discord.Color.red()
+            )
+            embed.set_footer(text="Bisonun keyfi 🐾")
+            await ctx.send(embed=embed)
+            return
+
         jokes = self.load_jokes()  # Şakaları yükle
         joke = random.choice(jokes)  # Rastgele bir şaka seç
         await ctx.send(joke['joke'])  # Şakayı gönder
