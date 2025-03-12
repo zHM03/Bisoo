0cbd733022d4bab062f1207a042103293ce33715
Hisham <92490723+zHM03@users.noreply.github.com>
2025-03-12 06:28:43 +0300

    Update Cark.py

--- fun/Cark.py
+++ fun/Cark.py
@@ -16,8 +16,8 @@ class Cark(commands.Cog):
             await ctx.send("⚠️ En az 2 seçenek girmelisin! Örnek: `!çark a b c d e`")
             return
 
-        tur_sayısı = 2  # Kaç tam tur dönecek (Azaltıldı)
-        kare_sayısı = 15  # Animasyon için kare sayısı (Azaltıldı)
+        tur_sayısı = 5  # Kaç tam tur dönecek (Azaltıldı)
+        kare_sayısı = 10  # Animasyon için kare sayısı (Azaltıldı)
         açılar = np.linspace(0, 360 * tur_sayısı, kare_sayısı)  # Döndürme açıları
         son_açı = random.uniform(0, 360)  # Rastgele son durma açısı
         çark_açısı = açılar + son_açı
