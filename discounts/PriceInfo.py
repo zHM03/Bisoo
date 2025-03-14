e3955c7661d3f88cd9b93e97cb0b78d8c44348f3
Hisham <92490723+zHM03@users.noreply.github.com>
2025-03-14 07:34:49 +0300

    Update PriceInfo.py

--- discounts/PriceInfo.py
+++ discounts/PriceInfo.py
@@ -89,5 +89,5 @@ class SteamGame(commands.Cog):
 
         await ctx.send(embed=embed)
 
-def setup(bot):
-    bot.add_cog(SteamGame(bot))
+async def setup(bot):
+    await bot.add_cog(SteamGame(bot))
