ea6e7ea645790d651d66ed0f11a70992b724553c
Hisham <92490723+zHM03@users.noreply.github.com>
2025-03-14 03:16:58 +0300

    Update PriceInfo.py

--- discounts/PriceInfo.py
+++ discounts/PriceInfo.py
@@ -1,19 +1,21 @@
 import discord
 from discord.ext import commands
 import requests
+import urllib.parse
 
 class GamePriceCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="gameprice")
-    async def get_game_price(self, ctx, game_title: str):
+    async def get_game_price(self, ctx, *, game_title: str):
         """Belirtilen oyun için en iyi fiyatı gösterir"""
         
         # API anahtarınızı buraya ekleyin
         api_key = "37d8ca093b6022f360d8e48ce69932797bc3c4e2"
-        # URL'yi doğru şekilde oluşturun
-        url = f"https://api.isthereanydeal.com/lookup/game/title/{game_title}/?key={api_key}"
+        # Oyun başlığını URL'ye düzgün şekilde entegre et
+        game_title_encoded = urllib.parse.quote(game_title)  # Başlıkta boşlukları ve özel karakterleri encode et
+        url = f"https://api.isthereanydeal.com/lookup/game/title/{game_title_encoded}/?key={api_key}"
 
         # API isteği gönderme
         response = requests.get(url)
